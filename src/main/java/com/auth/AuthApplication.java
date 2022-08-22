56c37b138a467a3da1962261ef37dc53d4569c7f
Andika Octavia <andhika@actdisain.com>
2022-08-22 13:59:33 +0700

    remove unused function and code

--- src/main/java/com/auth/AuthApplication.java
+++ src/main/java/com/auth/AuthApplication.java
@@ -1,9 +1,5 @@
 package com.auth;
 
-import com.auth.model.Role;
-import com.auth.model.User;
-import com.auth.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,22 +15,6 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run(UserService userService) {
-
-		return args -> {
-
-			userService.saveUser(new User(null, "andikaopp", "andikaopp@gmail.com", "123456", "085645890621", 0, null, new ArrayList<>()));
-			userService.saveUser(new User(null, "julia", "julia@gmail.com", "123456", "085645890621", 0, null, new ArrayList<>()));
-
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
-			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-			userService.addRoleToUser("andikaopp", "ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("julia", "ROLE_ADMIN");
-		};
 	}
 
 	@Bean
